e991c1f24b2c390757652d2dd80677b09031574e
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-06-23 22:54:18 +0300

    Update train_classifiers.py
    - Add logging messages.
    - Add method call train_mlp_classifier.

--- scripts/train_classifiers.py
+++ scripts/train_classifiers.py
@@ -2,13 +2,16 @@ import numpy as np
 
 from dataset import load_audio_clips
 from feature_extraction import extract_features
+from mlp_classifier import train_mlp_classifier
 
 
 def main():
     # Load the audio clips
+    print('Loading audio clips...')
     foreground_audio_clips, background_audio_clips = load_audio_clips()
 
     # Extract features from the audio clips
+    print('Extracting features...')
     foreground_features = np.array([extract_features(audio_clip) for audio_clip in foreground_audio_clips])
     background_features = np.array([extract_features(audio_clip) for audio_clip in background_audio_clips])
 
@@ -23,6 +26,12 @@ def main():
     # Combine the foreground and background flattened features and labels
     features = np.concatenate((foreground_features_flattened, background_features_flattened))
     labels = np.concatenate((foreground_labels, background_labels))
+
+    print('Classifiers training started... This may take a while.')
+
+    # MLP (Multi-Layer Perceptron) classifier training
+    print('Training MLP classifier...')
+    train_mlp_classifier(features, labels)
 
 
 if __name__ == '__main__':
