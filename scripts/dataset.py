1dcdaeaef830a631bd7cfde1ffa820ed9a038646
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-06-25 23:33:16 +0300

    Update dataset.py
    - Change the audio clips loading path.
    - Rename functions.
    - Update docstrings.

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,61 +1,62 @@
 import os
+import sys
 
 import librosa
 
 
-def load_audio_clips():
+def load_train_audio_clips():
     """
-    Load foreground and background audio clips from the '../datasets/' directory.
+    Load foreground and background train audio clips from the '../datasets/train/' directory.
 
     :return: tuple
         A tuple containing two lists:
-        - List[np.ndarray]: Foreground audio clips.
-        - List[np.ndarray]: Background audio clips.
+        - List[np.ndarray]: Foreground train audio clips.
+        - List[np.ndarray]: Background train audio clips.
     """
-    # Load and return the foreground and background audio clips.
-    return load_foreground_audio_clips(), load_background_audio_clips()
+    # Load and return the foreground and background train audio clips.
+    return load_foreground_train_audio_clips(), load_background_train_audio_clips()
 
 
-def load_foreground_audio_clips():
+def load_foreground_train_audio_clips():
     """
-    Load all foreground audio clips from the '../datasets/foreground/clips' directory.
+    Load all foreground train audio clips from the '../datasets/train/foreground/clips' directory.
 
     :return: list
-        - List[numpy.ndarray]: Foreground audio clips.
+        - List[numpy.ndarray]: Foreground train audio clips.
     """
-    # Get all foreground audio clips names from the dataset.
-    foreground_audio_clips = os.listdir('../datasets/foreground/clips')
+    # Get all foreground train audio clips names from the dataset.
+    foreground_train_audio_clips = os.listdir('../datasets/train/foreground/clips')
 
-    # Load and return all foreground audio clips from the dataset.
-    return [load_audio_clip(audio_name, 'foreground') for audio_name in foreground_audio_clips]
+    # Load and return all foreground train audio clips from the dataset.
+    return [load_train_audio_clip(audio_name, 'foreground') for audio_name in foreground_train_audio_clips]
 
 
-def load_background_audio_clips():
+def load_background_train_audio_clips():
     """
-    Load all background audio clips from the '../datasets/background/clips' directory.
+    Load all background train audio clips from the '../datasets/train/background/clips' directory.
 
     :return: list
-        - List[np.ndarray]: Background audio clips.
+        - List[np.ndarray]: Background train audio clips.
     """
-    # Get all background audio clips from the dataset.
-    background_audio_clips = os.listdir('../datasets/background/clips')
+    # Get all background train audio clips from the dataset.
+    background_train_audio_clips = os.listdir('../datasets/train/background/clips')
 
-    # Load and return all background audio clips from the dataset.
-    return [load_audio_clip(audio_name, 'background') for audio_name in background_audio_clips]
+    # Load and return all background train audio clips from the dataset.
+    return [load_train_audio_clip(audio_name, 'background') for audio_name in background_train_audio_clips]
 
 
-def load_audio_clip(audio_name, directory):
+def load_train_audio_clip(audio_name, directory):
     """
-    Load an audio clip from 1.5 seconds to 3.5 seconds from the specified directory.
+    Load a train audio clip from 1.5 seconds to 3.5 seconds from the specified directory.
 
-    This function loads the audio data from the '../datasets/{directory}/clips/' directory.
+    This function loads the audio data from the '../datasets/train/{directory}/clips/' directory.
 
     :param audio_name: str
-        The name of the audio file to load.
+        The name of the train audio file to load.
     :param directory: str
-        The directory where the audio file is located. Expected values are 'foreground' or 'background'.
+        The directory where the train audio file is located. Expected values are 'foreground' or 'background'.
     :return: numpy.ndarray
-        The audio data as a numpy array.
+        The train audio data as a numpy array.
     """
-    audio_clip, _ = librosa.load(f'../datasets/{directory}/clips/{audio_name}', offset=1.5, duration=2)
+    audio_clip, _ = librosa.load(f'../datasets/train/{directory}/clips/{audio_name}', offset=1.5, duration=2)
     return audio_clip
