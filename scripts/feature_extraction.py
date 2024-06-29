f5542cb18a14644d32ed909fc0ba66cff4e713f0
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-06-29 15:07:05 +0300

    Update feature_extraction.py

--- scripts/feature_extraction.py
+++ scripts/feature_extraction.py
@@ -2,13 +2,15 @@ import librosa
 import numpy as np
 from librosa import feature
 
+from common import sr, n_fft, hop_length
+
 
 def extract_features(audio_data):
     """
     Compute the Mel-frequency spectrogram of the audio data and convert it to decibels units.
 
-    This function computes the Mel-frequency spectrogram of the input audio data using specified
-    parameters (window length, hop length, number of Mel bands), and converts it to decibels units
+    This function computes the Mel-frequency spectrogram of the input audio data using specified parameters
+    (sample rate, window length, hop length, number of Mel bands), and converts it to decibels units
     using logarithmic scaling.
 
     Returns the Mel-frequency spectrogram in decibels units, representing the extracted features.
@@ -18,9 +20,5 @@ def extract_features(audio_data):
     :return: numpy.ndarray
         The computed Mel-frequency spectrogram in decibels.
     """
-    n_fft = 1024  # Window length
-    hop_length = n_fft // 2  # Number of samples between successive frames
-    n_mels = 96  # Number of Mel bands to generate (filter bank size)
-    melspectrogram = librosa.feature.melspectrogram(y=audio_data, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
-
+    melspectrogram = librosa.feature.melspectrogram(y=audio_data, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=96)
     return librosa.power_to_db(melspectrogram, ref=np.max)
